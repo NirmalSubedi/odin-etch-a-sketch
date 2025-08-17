56ac1fc6d823a2e2d439cf51113b6b0fbfd07b1b
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2025-08-17 15:22:11 -0700

    add the ability to resize sketchpad
    Resize:
    - Max 100 squares per side allowed

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const resizeButton = document.createElement("button");
+let totalSquares = 256;
 
-for (let i = 1; i <= 256; i++) {
+function createSquares() {
+for (let i = 1; i <= totalSquares; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
     cell.addEventListener("mouseenter", event => event.target.style.backgroundColor = "black");
+    const squareSize = 100 / (Math.sqrt(totalSquares)); 
+    cell.style.width = `${squareSize}vw`;
+    cell.style.height = `${squareSize}vw`;
+}}
+
+function resizeSketchPad() {
+    const squaresPerSide = Number(prompt("How many squares per side?"));
+    totalSquares = squaresPerSide * squaresPerSide;
+    if (squaresPerSide <= 100)  {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createSquares();
+    }
 }
 
+resizeButton.textContent = "resize";
+body.insertBefore(resizeButton, container);
+resizeButton.addEventListener("click", resizeSketchPad)
+
+createSquares();
