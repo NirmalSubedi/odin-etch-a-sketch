62b5d155709e5148c13f28caa71ea18b5cd50cd0
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2025-08-17 14:09:57 -0700

    add hover effect
    -On hover the background color changes to black from grey

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const container = document.querySelector(".container");
 for (let i = 1; i <= 256; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
+    cell.addEventListener("mouseenter", event => event.target.style.backgroundColor = "black");
 }
+
